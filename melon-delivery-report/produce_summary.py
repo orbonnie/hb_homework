97d8546692678eec4073041fc54657292c259cb3
Bonnie Owens <arlolocos@gmail.com>
2022-11-03 18:36:36 -0400

    completed melon delivery report

---
+++ melon-delivery-report/produce_summary.py
@@ -0,0 +1,40 @@
+print("Day 1")
+the_file = open("um-deliveries-day-1.txt")
+for line in the_file:
+    line = line.rstrip()
+    words = line.split('|')
+
+    melon = words[0]
+    count = words[1]
+    amount = words[2]
+
+    print(f"Delivered {count} {melon}s for total of ${amount}")
+the_file.close()
+
+
+print("Day 2")
+the_file = open("um-deliveries-day-2.txt")
+for line in the_file:
+    line = line.rstrip()
+    words = line.split('|')
+
+    melon = words[0]
+    count = words[1]
+    amount = words[2]
+
+    print(f"Delivered {count} {melon}s for total of ${amount}")
+the_file.close()
+
+
+print("Day 3")
+the_file = open("um-deliveries-day-3.txt")
+for line in the_file:
+    line = line.rstrip()
+    words = line.split('|')
+
+    melon = words[0]
+    count = words[1]
+    amount = words[2]
+
+    print(f"Delivered {count} {melon}s for total of ${amount}")
+the_file.close()
